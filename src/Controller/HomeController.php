4b35bde3190453228488d3ffbe826c7683cac9c0
Cyril Vassallo <cyrilvssll34@gmail.com>
2019-08-07 11:09:30 +0200

    Premier commit sur SymBNB

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller{
+    
+
+    /**
+     * @Route("hello/{prenom}/age/{age}", name="hello")
+     * @Route("hello", name="hello_base")
+     * @Route("hello/{prenom}/age", name="hello_prenom")
+     * This function tell you who you are calling in Url
+     */
+    public function hello($prenom = "anonyme", $age = 0){
+        return $this->render('hello.html.twig', [
+            'prenom'=> $prenom,
+            'age' => $age
+        ]); 
+    }
+
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function home(){
+
+
+        return $this->render('home.html.twig');
+    }
+}
+
+
+?>
