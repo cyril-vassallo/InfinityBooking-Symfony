c77a418c8b9a4afd20bbfc735c43ae0c39f207da
Cyril Vassallo <cyrilvssll34@gmail.com>
2019-08-13 16:29:17 +0200

    Install form in controller

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -6,6 +6,7 @@ use App\Entity\Ad;
 use App\Repository\AdRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
@@ -23,12 +24,50 @@ class AdController extends AbstractController
     }
 
     /**
-     * Cette fonction affiche une seule annonce
+     *Permet de creer une annomce
+     *@Route("/ads/new",name="ads_create")
+     * @return Response
+     */
+    public function create(){
+
+        $ad = new Ad();
+        $form = $this->createFormBuilder($ad)
+            ->add('title')
+            ->add('introduction')
+            ->add('content')
+            ->add('rooms')
+            ->add('price')
+            ->add('coverImage')
+            ->add('save', SubmitType::class, [
+                'label' => 'Ajouter votre annonce',
+                'attr' => ['class'=> 'btn btn-primary']
+            ])
+            ->getForm();
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Cette fonction affiche une seule annonce grace au convert parametter
      *
      * @Route("/ads/{slug}", name="ads_show")
      * 
      * @return Response
      */
+
+        public function show(Ad $ad){
+        return $this->render('ad/show.html.twig',[
+
+            'ad'=> $ad
+        ]);
+    }
+
+
+    /*
+    *Function avec injection de dependence
+    *
     public function show($slug, adRepository $repo){
 
         $ad = $repo->findOneBySlug($slug);
@@ -38,6 +77,6 @@ class AdController extends AbstractController
             'ad'=> $ad
         ]);
     }
-
+    */
 
 }
