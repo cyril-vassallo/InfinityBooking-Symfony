efb7e523b66a403651fbc2e5015382e9b80574ef
zhangsaili <cyrilvssll34@gmail.com>
2020-04-10 18:41:08 +0200

    Ajout des messages homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\AdRepository;
 use App\Repository\UserRepository;
+use Egulias\EmailValidator\Warning\Warning;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -13,6 +14,16 @@ class HomeController extends AbstractController{
      * @Route("/", name="homepage")
      */
     public function home(AdRepository $adRepo, UserRepository $userRepo){
+        $this->addFlash(
+            'danger',
+            "Attention !! Ce site n'est pas un réel site de réservation. Il est une démonstration pour portfolio développeur. Cette application tourne sur <a href='https://symfony.com/'><strong> Symfony 4.3 </strong></a> installé dans un service container de Heroku !"
+        );
+        $this->addFlash(
+            'warning',
+            "Certaines des images peuvent ne pas s'afficher correctement car les sources proviennent de l'api <a href='http://lorempixel.com/'><strong> lorempixel.com </strong> </a>très largement sollicitée par la communauté des développeurs, merci de votre comprehension."
+        );
+
+
         return $this->render('home.html.twig',[
             'bestAds' => $adRepo->findBestAds(3),
             'bestUsers' => $userRepo->findBestUsers()
